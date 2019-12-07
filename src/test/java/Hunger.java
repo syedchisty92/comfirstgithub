ae0b3e55359298afe17a51b1b5f17d58eaa29478
csyed <csyed@deloitte.com>
2019-12-07 19:25:24 +0530

    second modified

--- src/test/java/Hunger.java
+++ src/test/java/Hunger.java
@@ -2,6 +2,16 @@ public class Hunger {
 
     public static void main(String[] args)
     {
+        int i=1;
         System.out.println("hie");
+        System.out.println("vool");
+        if(i==1)
+        {
+            System.out.println("correct");
+        }
+        else
+        {
+            System.out.println("wrong");
+        }
     }
 }
